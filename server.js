0b36902c76c2fc019b40fd7677eb02db8543f281
Relliner <41902228+rehlingera@users.noreply.github.com>
2019-02-06 11:56:35 -0600

    fixed database connection

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to database
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 
 
 
